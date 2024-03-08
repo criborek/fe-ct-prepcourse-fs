58746c9042ec295a02b4dc2dbe3603a2eab3979b
Cristian <hernanvitola7@gmail.com>
2024-03-08 12:53:06 +0000

    test.js

--- C5 Fundamentos Javascript/ejercicios/14.js
+++ C5 Fundamentos Javascript/ejercicios/14.js
@@ -3,6 +3,12 @@ function tienenMismaLongitud(str1, str2) {
   // Retorna true si los dos strings tienen la misma longitud.
   // De lo contrario, retorna false.
   // Tu código:
+  var stringLongitud1 = str1.length;
+  var stringLongitud2 = str2.length;
+  if (stringLongitud1 === stringLongitud2){
+    return true;
+  }else {return false;
+  }
 }
 
 module.exports = tienenMismaLongitud;
