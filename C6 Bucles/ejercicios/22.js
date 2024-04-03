30abf71b599762572976448321516e249e277e8f
Cristian <hernanvitola7@gmail.com>
2024-04-03 05:05:05 +0100

    modulo bucles

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -5,7 +5,47 @@ function combine(str1, str2, str3) {
    // EJEMPLOS
    // combine("abc", "", "123") == "a1b2c3"
    // combine("abc", "12345", "") == "a1b2c345"
-   // combine("abc", "12345", "67") == "a16b27c345"
-}
+   // combine("abc", "12345", "67") == "a16b27c345"function combinar(str1, str2, str3)
 
-module.exports = combine;
+
+
+   
+      // Inicializamos una cadena vacía para almacenar el resultado combinado.
+      let result = "";
+  
+      // Iteramos sobre la longitud máxima entre las tres cadenas.
+      for (let i = 0; i < Math.max(str1.length, str2.length, str3.length); i++) {
+          // Verificamos si el carácter en la posición actual de la cadena str1 no es undefined y no está vacío.
+          if (str1[i] !== undefined && str1[i] !== '') {
+              // Si es así, agregamos este carácter al resultado.
+              result += str1[i];
+          }
+  
+          // Repetimos el mismo proceso para la cadena str2.
+          if (str2[i] !== undefined && str2[i] !== '') {
+              result += str2[i];
+          }
+  
+          // Repetimos el mismo proceso para la cadena str3.
+          if (str3[i] !== undefined && str3[i] !== '') {
+              result += str3[i];
+          }
+      }
+  
+      // Devolvemos el resultado final.
+      return result;
+  }
+  
+console.log(combine("hola", "123", ""));
+module.exports= combine;
+ 
+
+   
+
+
+
+
+
+
+
+
